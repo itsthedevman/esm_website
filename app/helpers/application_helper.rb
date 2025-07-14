52543b1a4886b59b25b180cee19f8eea18c79a96
Bryan <bryan@itsthedevman.com>
2025-07-13 21:17:14 -0400

    Refactor render_component method to improve component initialization and loading

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,8 +2,10 @@
 
 module ApplicationHelper
   def render_component(klass, **locals, &block)
-    render klass.new(current_user:, &block).tap do |component|
-      component.call(**locals) if component.respond_to?(:call)
+    component = klass.new(current_user:, &block).tap do |component|
+      component.on_load(**locals) if component.respond_to?(:on_load)
     end
+
+    render(component)
   end
 end
