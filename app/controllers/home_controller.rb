822b33820451534256f3d9cd58366d14452b29b9
Bryan <bryan@itsthedevman.com>
2025-07-13 21:48:15 -0400

    Refactor toast component and styles for improved structure and clarity

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,10 @@
 
 class HomeController < ApplicationController
   def index
+    flash[:info] = "Hello!"
     flash[:success] = "Hello!"
+    flash[:warn] = "Hello!"
+    flash[:error] = "Hello!"
     command_count = ESM::CommandDetail.all.size
     render locals: {command_count:}
   end
