5bf6d888b4eb9b03c10cb8843c7482f36b5c9e3b
Bryan <bryan@itsthedevman.com>
2025-07-21 22:30:57 -0400

    Register RoleSelectorController and update SCSS imports for better organization; add Choices.js dependency for enhanced role selection functionality.

--- app/frontend/controllers/index.js
+++ app/frontend/controllers/index.js
@@ -6,3 +6,6 @@ import { application } from "./application"
 
 import NotificationsController from "./notifications_controller"
 application.register("notifications", NotificationsController)
+
+import RoleSelectorController from "./role_selector_controller"
+application.register("role-selector", RoleSelectorController)
---
+++ app/frontend/controllers/role_selector_controller.ts
@@ -0,0 +1,17 @@
+import { Controller } from "@hotwired/stimulus";
+import Choices from "choices.js";
+import "choices.js/public/assets/styles/choices.css";
+
+// Connects to data-controller="role-selector"
+export default class extends Controller {
+  connect() {
+    const element = this.element as HTMLElement;
+    if (element.classList.contains("choices__input")) return;
+
+    const rolesJSON = element.dataset.roles;
+    if (rolesJSON == null || rolesJSON.length === 0) return;
+
+    const roles = JSON.parse(rolesJSON);
+    new Choices(element, { choices: roles, allowHTML: true });
+  }
+}
