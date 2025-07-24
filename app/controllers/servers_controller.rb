94ca4cdc7f49c5802bcb453092feac6f023e5f42
Bryan <bryan@itsthedevman.com>
2025-07-23 23:59:43 -0400

    Refactor server management views: Implement edit and new server forms, enhance layout and visibility options, and remove obsolete show view.

--- app/controllers/servers_controller.rb
+++ app/controllers/servers_controller.rb
@@ -4,6 +4,10 @@ class ServersController < AuthenticatedController
   before_action :check_for_community_access!
 
   def show
+    redirect_to edit_community_server_path(current_community, params[:server_id])
+  end
+
+  def edit
     server = current_community.servers.find_by(public_id: params[:server_id])
 
     render locals: {server:}
