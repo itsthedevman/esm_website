293757d25ebb0e8d04d0f8c20f4973d08e229cf1
Bryan <bryan@itsthedevman.com>
2025-07-26 01:12:40 -0400

    feat: Add welcome message toggle functionality in community edit form

--- app/frontend/controllers/edit_community_controller.js
+++ app/frontend/controllers/edit_community_controller.js
@@ -1,6 +1,7 @@
 import { Controller } from "@hotwired/stimulus";
 import JustValidate from "just-validate";
 import { allowTurbo } from "../helpers/just_validate";
+import $ from "cash-dom";
 
 // Connects to data-controller="edit-community"
 export default class extends Controller {
@@ -11,6 +12,13 @@ export default class extends Controller {
 
     this.#initializeValidator();
     allowTurbo(this.validator);
+  }
+
+  onWelcomeMessageToggleClick(event) {
+    const toggleElem = $(event.currentTarget);
+    const messageBoxElem = $("#community_welcome_message");
+
+    messageBoxElem.prop("disabled", !toggleElem.is(":checked"));
   }
 
   //////////////////////////////////////////////////////////////////////////////
