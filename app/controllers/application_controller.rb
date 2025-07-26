99a40ed8e4d90abf442954c2c8cfe4d88eec0917
Bryan <bryan@itsthedevman.com>
2025-07-26 00:01:24 -0400

    feat: Add ToastHelper for creating toast notifications and delegate methods in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,9 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  delegate :create_toast,
+    :create_info_toast, :create_success_toast,
+    :create_warn_toast, :create_error_toast,
+    to: :helpers
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,17 +9,6 @@ module ApplicationHelper
     render(component)
   end
 
-  def create_toast(body, title: "", subtitle: "")
-    turbo_stream.append("toast-container") do
-      concat(
-        render_component(
-          ToastComponent,
-          title: title, subtitle: subtitle, body: body
-        )
-      )
-    end
-  end
-
   def update_turbo_modal(&block)
     turbo_frame_tag "turbo_modal" do
       yield
