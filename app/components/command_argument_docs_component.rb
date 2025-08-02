12958fc8fd44e8071acb6c0136ed5a8af9570c01
Bryan <bryan@itsthedevman.com>
2025-08-02 00:09:22 -0400

    refactor: optimize argument rendering by adding index-based margin handling

--- app/components/command_argument_docs_component.rb
+++ app/components/command_argument_docs_component.rb
@@ -8,12 +8,15 @@ class CommandArgumentDocsComponent < ApplicationComponent
   end
 
   def call
+    arguments_size = @command.arguments.size
+
     arguments =
-      @command.arguments.join_map do |name, argument|
+      @command.arguments.join_map(with_index: true) do |(name, argument), index|
         semantic_class = argument_semantic_class(name, argument)
+        bottom_margin = (arguments_size == (index + 1)) ? "" : "mb-3"
 
         <<~HTML
-          <div class="mb-3 p-3 bg-dark border border-secondary rounded">
+          <div class="p-3 bg-dark border border-secondary rounded #{bottom_margin}">
             <div class="mb-2">
               <span class="arg #{semantic_class} fs-6">#{argument["display_name"]}</span>
             </div>
