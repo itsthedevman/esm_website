8eabb9e173b105e21b9d610d04a9c308183fb659
Bryan <bryan@itsthedevman.com>
2025-08-04 23:39:45 -0400

    feat: enhance notification creation modal with live preview and color selection functionality

--- app/frontend/controllers/notification_new_controller.js
+++ app/frontend/controllers/notification_new_controller.js
@@ -1,19 +1,58 @@
 import ApplicationController from "./application_controller";
 import $ from "../helpers/cash_dom";
+import Markdown from "../helpers/markdown";
 
 // Connects to data-controller="notification-new"
 export default class extends ApplicationController {
-  static targets = ["colorPicker"];
+  static targets = ["colorSelect", "colorPicker", "livePreview"];
 
-  connect() {}
+  connect() {
+    this.preview = {
+      color: $(this.colorSelectTarget).val(),
+      title: "Preview title will appear here",
+      description: "Preview message will appear here",
+      footer: "[esm] Exile Server Manager",
+    };
 
-  onColorChanged(event) {
-    const value = $(event.currentTarget).val();
+    this.#renderLivePreview();
+  }
 
-    if (value == "custom") {
-      $(this.colorPickerTarget).show();
+  onColorChanged(_event) {
+    const selectedColor = $(this.colorSelectTarget).val();
+    const colorPicker = $(this.colorPickerTarget);
+
+    // Toggle the color picker
+    if (selectedColor == "custom") {
+      colorPicker.show();
+
+      this.preview.color = colorPicker.val();
     } else {
-      $(this.colorPickerTarget).hide();
+      colorPicker.hide();
+
+      this.preview.color = selectedColor;
+    }
+
+    this.#renderLivePreview();
+  }
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////
+
+  #renderLivePreview() {
+    const preview = $(this.livePreviewTarget);
+
+    preview.find("#title").html(Markdown.toHTML(this.preview.title));
+
+    preview
+      .find("#description")
+      .html(Markdown.toHTML(this.preview.description));
+
+    preview.find("#footer").html(this.preview.footer);
+
+    if (this.preview.color == "random") {
+      preview.addClass("random");
+    } else {
+      preview.removeClass("random");
+      preview.css("border-left-color", this.preview.color);
     }
   }
 }
