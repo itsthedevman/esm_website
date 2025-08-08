2187df6ecf0ff1918fe2141d7156ee537dc2f430
Bryan <bryan@itsthedevman.com>
2025-08-07 23:07:43 -0400

    feat: implement abort controller for event listeners in application controller

--- app/frontend/src/controllers/application_controller.js
+++ app/frontend/src/controllers/application_controller.js
@@ -1,6 +1,21 @@
 import { Controller } from "@hotwired/stimulus";
+import $ from "../helpers/cash_dom";
 
 export default class ApplicationController extends Controller {
+  initialize() {
+    this.abortController = new AbortController();
+  }
+
+  disconnect() {
+    this.abortController.abort();
+  }
+
+  addEventListener(selector, event, callback) {
+    $(selector)[0].addEventListener(event, callback, {
+      signal: this.abortController.signal,
+    });
+  }
+
   nextTick(callback) {
     setTimeout(callback, 0);
   }
