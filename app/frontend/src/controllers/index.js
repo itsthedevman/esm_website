da58f4e1515aa5e88791560564d32ee4d5eb7676
Bryan <bryan@itsthedevman.com>
2025-08-23 17:18:32 -0400

    feat: refactor controller registration to dynamically load all controllers

--- app/frontend/src/controllers/index.js
+++ app/frontend/src/controllers/index.js
@@ -1,43 +1,13 @@
 import { application } from "./application";
 
-import AdditionalLogsController from "./additional_logs_controller";
-application.register("additional-logs", AdditionalLogsController);
+const controllerModules = import.meta.glob("./*_controller.js", {
+  eager: true,
+});
 
-import AliasesController from "./aliases_controller";
-application.register("aliases", AliasesController);
+for (const [path, module] of Object.entries(controllerModules)) {
+  const controllerName = path
+    .match(/\.\/(.+)_controller\.js$/)[1]
+    .replace(/_/g, "-");
 
-import CollapseToggleController from "./collapse_toggle_controller";
-application.register("collapse-toggle", CollapseToggleController);
-
-import CommandConfigurationController from "./command_configuration_controller";
-application.register("command-configuration", CommandConfigurationController);
-
-import CommunityEditController from "./community_edit_controller";
-application.register("community-edit", CommunityEditController);
-
-import NotificationsController from "./notifications_controller";
-application.register("notifications", NotificationsController);
-
-import RoleSelectorController from "./role_selector_controller";
-application.register("role-selector", RoleSelectorController);
-
-import RptParserController from "./rpt_parser_controller";
-application.register("rpt-parser", RptParserController);
-
-import ServerEditController from "./server_edit_controller";
-application.register("server-edit", ServerEditController);
-
-import ServerModsController from "./server_mods_controller";
-application.register("server-mods", ServerModsController);
-
-import ServerNewController from "./server_new_controller";
-application.register("server-new", ServerNewController);
-
-import ServerRewardsController from "./server_rewards_controller";
-application.register("server-rewards", ServerRewardsController);
-
-import SlimSelectController from "./slim_select_controller";
-application.register("slim-select", SlimSelectController);
-
-import ToastsController from "./toasts_controller";
-application.register("toasts", ToastsController);
+  application.register(controllerName, module.default);
+}
