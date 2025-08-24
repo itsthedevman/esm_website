f77f55c4d35ad8424553db15ea8146412bcb0508
Bryan <bryan@itsthedevman.com>
2025-08-24 00:16:01 -0400

    feat: add rescue handlers for improved error management in controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@
 
 class ApplicationController < ActionController::Base
   include Exceptions
+  include RescueHandlers
 
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
@@ -11,22 +12,6 @@ class ApplicationController < ActionController::Base
     :create_warn_toast, :create_error_toast,
     :hide_modal,
     to: :helpers
-
-  rescue_from NotFoundError, with: :render_not_found
-  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
-
-  def render_not_found(exception = nil)
-    respond_to do |format|
-      format.html { render template: "errors/not_found_404", status: :not_found }
-
-      format.json { render json: {error: "Not found"}, status: :not_found }
-
-      format.turbo_stream do
-        render turbo_stream: create_error_toast("The request item was not found"),
-          status: :not_found
-      end
-    end
-  end
 
   def not_found!
     raise NotFoundError.new
