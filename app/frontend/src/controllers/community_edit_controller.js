cb3db9ea8e72060ebddde44358d0cae3240afa02
Bryan <bryan@itsthedevman.com>
2025-08-30 01:16:27 -0400

    refactor: enhance welcome message section with improved UI and toggle functionality

--- app/frontend/src/controllers/community_edit_controller.js
+++ app/frontend/src/controllers/community_edit_controller.js
@@ -5,7 +5,12 @@ import $ from "../helpers/cash_dom";
 
 // Connects to data-controller="community-edit"
 export default class extends ApplicationController {
-  static targets = ["form", "welcomeMessageLength"];
+  static targets = [
+    "form",
+    "welcomeMessageLength",
+    "welcomeMessageEnabled",
+    "welcomeMessageDisabled",
+  ];
 
   connect() {
     this.validator = new JustValidate(this.formTarget);
@@ -19,7 +24,19 @@ export default class extends ApplicationController {
     const toggleElem = $(event.currentTarget);
     const messageBoxElem = $("#community_welcome_message");
 
-    messageBoxElem.prop("disabled", !toggleElem.is(":checked"));
+    const disabled = !toggleElem.is(":checked");
+    messageBoxElem.prop("disabled", disabled);
+
+    const enabledCardElem = $(this.welcomeMessageEnabledTarget);
+    const disabledCardElem = $(this.welcomeMessageDisabledTarget);
+
+    if (disabled) {
+      enabledCardElem.hide();
+      disabledCardElem.show();
+    } else {
+      enabledCardElem.show();
+      disabledCardElem.hide();
+    }
   }
 
   onWelcomeMessageInput(event) {
